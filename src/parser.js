b4b3b529e2e8e0c12be98c844b87783faa029355
CJ Melegrito <cj@mlgrto.com>
2017-08-06 04:33:27 +1000

    Use alt index and prep api for use

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,7 @@
 const read = require('node-readability')
 const strip = require('sanitize-html')
 
-module.exports.parser = function(url) {
+module.exports = function(url) {
 	read(url, function(error, article, meta) {
 		const text = strip(article.content, { allowedTags: [] })
 		return text
