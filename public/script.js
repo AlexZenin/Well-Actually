9bc8717d3e32d66adaaeb70f511fb9272392166c
Alex <ayzen1@student.monash.edu>
2017-08-06 09:46:40 +1000

    Add beautiful tag for sentiment

--- public/script.js
+++ public/script.js
@@ -18,7 +18,21 @@ $(function() {
 
 				// Loop over results and display them
 				for (index in results) {
-					$('#results').append('<div class="column"><div class="ui fluid card"><div class="image"><img src="' + results[index].imageURL + '"></div><div class="content"><a href="' + results[index].title + '" class="header">' + results[index].title + '</a><div class="description">' + results[index].content + '</div></div><div class="extra content"><span class="right floated">DATE</span><span><i class="user icon"></i>' + results[index].source + '</span></div></div></div>')
+
+					// Configure the tag with the right colour and sentiment value
+					var articleSentiment = results[index].sentiment;
+					var articleSentimentColour;
+					if (articleSentiment < 0.33){
+						articleSentimentColour = "red"
+					} else if (articleSentiment > 0.66){
+						articleSentimentColour = "green"
+					} else {
+						articleSentimentColour = "yellow"
+					}
+
+					var sentimentTag = '<div class="ui ' + articleSentimentColour + ' horizontal label">' + String(articleSentiment) + '</div>'
+
+					$('#results').append('<div class="column"><div class="ui fluid card"><div class="image"><img src="' + results[index].imageURL + '"></div><div class="content"><a href="' + results[index].title + '" class="header">' + results[index].title + '</a><div class="description">' + results[index].content + '</div></div><div class="extra content"><span class="right floated">' + sentimentTag + '</span><span><i class="user icon"></i>' + results[index].source + '</span></div></div></div>')
 				}
 			})
 
