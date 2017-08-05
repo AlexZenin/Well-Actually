ba633d169b6e0265c2c5565aaa8a60c05c4a5daa
CJ Melegrito <cj@mlgrto.com>
2017-08-06 07:31:34 +1000

    Got images and multiple sources to work

--- src/suggest.js
+++ src/suggest.js
@@ -4,7 +4,6 @@ const bestImage = require('best-image')
 const sources = [
 	'http://feeds.nature.com/NatureNewsComment',
 	'http://www.snopes.com/feed/',
-	'https://www.sciencedaily.com/rss/all.xml',
 	'http://www.abc.net.au/news/feed/52278/rss.xml'
 ]
 
@@ -16,26 +15,36 @@ const options = {
 
 module.exports = function (keywords, callback) {
 	articles = []
+	total = 0
 
-	rssParser.parseURL(sources[0], function(error, parsed) {
-		parsed.feed.entries.forEach(function(entry) {
-			articles.push({
-				'title': entry.title,
-				'url': entry.link,
-				'content': entry.content,
-				'imageURL': ''
+	sources.forEach(function(source) {
+
+		rssParser.parseURL(source, function(error, parsed) {
+			parsed.feed.entries.forEach(function(entry) {
+				articles.push({
+					'title': entry.title,
+					'url': entry.link,
+					'content': entry.content,
+					'imageURL': '',
+					'source': parsed.feed.title
+				})
 			})
-		})
 
-		relevant = articles.filter(function(article) {
-			for (index in keywords) {
-				if (article.title.toLowerCase().indexOf(keywords[index]) > -1) {
-					return true
+			relevant = articles.filter(function(article) {
+				for (index in keywords) {
+					if (article.title.toLowerCase().indexOf(keywords[index]) > -1) {
+						return true
+					}
 				}
+				return false
+			})
+
+			total += 1
+
+			if (total >= sources.length) {
+				return callback(null, relevant)
 			}
-			return false
 		})
 
-		return callback(null, relevant)
 	})
 }
