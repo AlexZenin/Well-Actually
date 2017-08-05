2bea2519d189745b7ad5fa364c5c21ddd565322b
CJ Melegrito <cj@mlgrto.com>
2017-08-05 21:45:47 +1000

    Finalise post request

--- public/script.js
+++ public/script.js
@@ -6,13 +6,10 @@ $(function() {
 
 		// Grab value from text box
 		const url = $('#user_input').val()
-		console.log(url)
 
-		// Display result from server
-		$.get('/test', function(data) {
-
-			// Parse GET request
-			const results = JSON.parse(data)
+		// Send the url to the server then parse the server's response
+		$.post('/post', { url: url }, function(response) {
+			const results = JSON.parse(response)
 
 			$('#results').show()
 
@@ -20,7 +17,6 @@ $(function() {
 			for (index in results) {
 				$('#results').append('<div class="ui segment"><a href="' + results[index].url + '">' + results[index].title + '</a></div>')
 			}
-
 		})
 	})
 })
