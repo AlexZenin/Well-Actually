f06cdf37a79b4bbc575b4cc18584d00ed46a2c57
Alexander <alexander@dyn-118-139-34-187.its.monash.edu.au>
2017-08-05 21:41:32 +1000

    Add reponsive post request

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require("body-parser")
+const http = require("http")
 const app = express()
 
 app.set('port', (process.env.PORT) || 8080 )
@@ -27,7 +29,34 @@ app.get('/test', function(req, res) {
 	res.end(JSON.stringify(source))
 })
 
+//********************/
+// Azure api calls
+//********************/
+
+//Here we are configuring express to use body-parser as middle-ware.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Header information
+
+var azure_options = {
+	"Ocp-Apim-Subscription-Key": "13b99fb3f346435d863bc48b933d8ab6",
+	"Content-Type": "application/json",
+	"Host": "westus.api.cognitive.microsoft.com"
+};
+
+// Main post request for processing data 
+
+app.post('/process', function(req, res){
+	console.log(req.body.text);
+	res.end("you hit it!")
+})
+
+
+//********************/
 // Start the app
+//********************/
 
 app.listen(app.get('port'), function() {
 	console.log('Server running on port', app.get('port'))
